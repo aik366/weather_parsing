cf6321229915635e952b7470503be7d99120bb39
Hayk <haykooo3@gmail.com>
2024-07-06 13:30:01 +0300

    commit 2

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ from bs4 import BeautifulSoup as BS
 
 def main():
     url = 'https://rp5.ru/Погода_в_Краснодаре,_Краснодарский_край'
-    class_ = 'ArchiveTemp'
 
     r = requests.get(url)
     html = BS(r.text, 'html.parser')
-    t = html.find(class_=class_).find(class_='t_0').text
     find_text = html.find('div', {'id': "forecastShort-content"}).find(class_='t_0').get_text()
 
     print(find_text[:-3])
